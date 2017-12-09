ae0f8f6f2445a1e5c2b1234e141ed3506f311b65
Yujie Xia <xiayjchn@gmail.com>
2017-12-09 10:17:57 +0800

    replace some config with environment variables

--- config.py
+++ config.py
@@ -2,16 +2,17 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?R'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.dirname(__file__) + '/data.sqlite'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'A0Zr98j/3yX R~XHH!jmN]LWX/,?R'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.dirname(__file__) + '/data.sqlite'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BOOTSTRAP_SERVE_LOCAL = True
     FLASKY_ADMIN = 'admin'
     FLASKY_FOLLOWERS_PER_PAGE = 20
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-    VJUDGE_REMOTE_URL = 'http://'
+    CELERY_BROKER_URL =os.environ.get('CELERY_BROKER_URL') or 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND =os.environ.get('CELERY_RESULT_BACKEND') or 'redis://localhost:6379/0'
+    VJUDGE_REMOTE_URL = os.environ.get('VJUDGE_REMOTE_URL') or 'http://localhost'
 
 
 class DevelopmentConfig(Config):
@@ -30,5 +31,5 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-    'default': ProductionConfig
+    'default': DevelopmentConfig
 }
