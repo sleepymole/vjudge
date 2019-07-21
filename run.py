3dc73108db374adfdbb83952f46dbc5826f94208
Yujie Xia <xiayjchn@gmail.com>
2019-07-21 17:12:13 +0800

    increase celery concurrency

--- run.py
+++ run.py
@@ -11,7 +11,8 @@ try:
         if flask_process.poll() == 0:
             flask_process = subprocess.Popen(shlex.split('gunicorn -w 2 -k gevent manage:app'))
         if celery_process.poll() == 0:
-            celery_process = subprocess.Popen(shlex.split('celery worker -l INFO -A manage.celery --beat'))
+            celery_process = subprocess.Popen(
+                shlex.split('celery worker --app=manage.celery -l info --concurrency=4 --beat'))
 except KeyboardInterrupt:
     flask_process.terminate()
     celery_process.terminate()
