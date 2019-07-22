e23ad7f654cc9a8218257437f44e5ae1bb896c2e
Yujie Xia <xiayjchn@gmail.com>
2019-07-23 00:11:51 +0800

    add argparse for run

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
-import shlex
-import subprocess
 import time
 
-flask_process = subprocess.Popen(shlex.split('gunicorn -w 2 -k gevent manage:app'))
+import argparse
+import shlex
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', required=False, dest='address', default='localhost:8000', help='address to bind')
+args = parser.parse_args()
+
+flask_process = subprocess.Popen(shlex.split(f"gunicorn -w 2 -k gevent manage:app -b '{args.address}'"))
 celery_process = subprocess.Popen(shlex.split('celery worker --app=manage.celery -l info --concurrency=8 --beat'))
 
 try:
     while True:
         time.sleep(3600)
         if flask_process.poll() == 0:
-            flask_process = subprocess.Popen(shlex.split('gunicorn -w 2 -k gevent manage:app'))
+            flask_process = subprocess.Popen(shlex.split(f"gunicorn -w 2 -k gevent manage:app -b '{args.address}'"))
         if celery_process.poll() == 0:
             celery_process = subprocess.Popen(
                 shlex.split('celery worker --app=manage.celery -l info --concurrency=8 --beat'))
